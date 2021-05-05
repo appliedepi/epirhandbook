e9e5874ea6e72ef848a0dd18b3b8f9af650e1090
Isaac Florence <30433879+isaac-florence@users.noreply.github.com>
2021-05-05 23:45:53 +0100

    recreate get_data function to get all or some files (grouped as appropriate)

--- R/get_data.R
+++ R/get_data.R
@@ -2,17 +2,40 @@
 #'
 #' Access data used in examples in the Handbook
 #'
-#' @param path Path on your computer where the file(s) should be saved to *string*
-#' @param file Name and file extension of the data to be downloaded *string*
-#' @param all Save all the handbook data files? *Boolean (TRUE/FALSE)*
+#' @param file Name and file extension of the data to be downloaded *string* or "ALL" which will save all Epidemiology R Handbook example files
+#' @param path **Default opens a directory picker interactively**. Path on your computer where the file(s) should be saved to *string*
 #'
 #' @export
-get_data <- function(path = NULL, file = NULL, all = FALSE){
+get_data <- function(file = NULL, path = rstudioapi::selectDirectory()){
 
-  stopifnot(!is.null(path))
+  ## must select a file
+  stopifnot(!is.null(file))
 
+  ## normalise in case given manually
   path <- normalizePath(path)
 
+  ## load file_lkup
+  file_lkup <- readRDS(system.file(package = "handbookfiles", "file_lookup.rds"))
 
+  ## check file looked for is in file_lkup
+  if(file %in% file_lkup$name){
+
+    ## file group
+    group <- file_lkup[which(file_lkup$name == file), "file_group"]
+
+    ## path to file
+    files_to_copy <- file_lkup[which(file_lkup$file_group == group), "path"]
+
+  } else if (file == "all") {
+
+    files_to_copy <- file_lkup$path
+
+  } else stop("You must choose a filename shown in the handbook or \"all\"")
+
+  # files_to_copy <- lapply(files_to_copy, system.file, package = "handbookfiles")
+
+  files_to_copy <- system.file(package = "handbookfiles", files_to_copy)
+
+  lapply(files_to_copy, file.copy, to = path)
 
 }
