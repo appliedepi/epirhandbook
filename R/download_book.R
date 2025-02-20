f83e59a26c0130cd61da0cd77a6c0b1ee55ed3ed
mdgrs <marc@arcium.com>
2025-02-20 18:08:47 +0100

    fix download link

--- R/download_book.R
+++ R/download_book.R
@@ -3,17 +3,20 @@
 #' @description Need to refer to the Epidemiology R Handbook offline in the field?
 #' Easily download the handbook in advance with this function
 #'
+#' @param lang language for the file to download. Defaults to 'en'
 #' @param save_loc **Run with no arguments to pick a directory interactively** Directory the book should be downloaded to
 #'
 #' @export
-download_book <- function(save_loc = rstudioapi::selectDirectory()){
+download_book <- function(lang = "en", save_loc = rstudioapi::selectDirectory()){
 
   stopifnot(!is.null("path"))
 
   save_file <- paste0(save_loc, "/Epi_R_Handbook_offline.html")
 
-  download.file(url = "https://github.com/epirhandbook/Epi_R_handbook/raw/master/offline_long/Epi_R_Handbook_offline.html",
-                destfile = save_file)
+  url_list = c("https://github.com/appliedepi/epiRhandbook_eng/raw/master/offline_long/index.", lang, ".html")
+  url = paste(url_list, collapse='')
+
+  download.file(url = url, destfile = save_file)
 
   cli::cli_alert_success("Complete! The Offline version of the hanbook is available here: {save_loc}")
 
